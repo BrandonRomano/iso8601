2ef8540f216c9c1c6768cbff3cfbbe7b0196c05e
BrandonRomano <BrandonRRomano@gmail.com>
2016-12-19 17:49:46 -0500

    Add null support

--- iso8601.go
+++ iso8601.go
@@ -2,45 +2,75 @@
 // ISO 8601 format, without subsecond resolution or time zone info.
 package iso8601
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"time"
+)
 
 const Format = "2006-01-02T15:04:05"
 const jsonFormat = `"` + Format + `"`
 
 var fixedZone = time.FixedZone("", 0)
 
-type Time time.Time
+type Time struct {
+	Time  time.Time
+	Valid bool
+}
 
 // New constructs a new iso8601.Time instance from an existing
 // time.Time instance.  This causes the nanosecond field to be set to
 // 0, and its time zone set to a fixed zone with no offset from UTC
 // (but it is *not* UTC itself).
 func New(t time.Time) Time {
-	return Time(time.Date(
-		t.Year(),
-		t.Month(),
-		t.Day(),
-		t.Hour(),
-		t.Minute(),
-		t.Second(),
-		0,
-		fixedZone,
-	))
+	return Time{
+		Time: time.Date(
+			t.Year(),
+			t.Month(),
+			t.Day(),
+			t.Hour(),
+			t.Minute(),
+			t.Second(),
+			0,
+			fixedZone,
+		),
+		Valid: true,
+	}
 }
 
 func (it Time) MarshalJSON() ([]byte, error) {
-	return []byte(time.Time(it).Format(jsonFormat)), nil
+	if !it.Valid {
+		return []byte("null"), nil
+	}
+	return []byte(time.Time(it.Time).Format(jsonFormat)), nil
 }
 
 func (it *Time) UnmarshalJSON(data []byte) error {
-	t, err := time.ParseInLocation(jsonFormat, string(data), fixedZone)
-	if err == nil {
-		*it = Time(t)
+	var err error
+	var v interface{}
+	if err = json.Unmarshal(data, &v); err != nil {
+		return err
 	}
-
+	switch x := v.(type) {
+	case string:
+		t, err := time.ParseInLocation(jsonFormat, x, fixedZone)
+		if err == nil {
+			it = &Time{
+				Time: t,
+				Valid: true,
+			}
+		}
+	case nil:
+		it.Valid = false
+		return nil
+	default:
+		err = fmt.Errorf("json: cannot unmarshal %v into Go value of type iso8601.Time", reflect.TypeOf(v).Name())
+	}
+	it.Valid = err == nil
 	return err
 }
 
 func (it Time) String() string {
-	return time.Time(it).String()
+	return time.Time(it.Time).String()
 }
